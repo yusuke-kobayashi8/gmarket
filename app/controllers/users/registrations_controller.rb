7ebcc30919bc78fc75d593b1cf14e5dd47bf7cb0
masayuki-tech <tennkuuno6@gmail.com>
2020-04-03 19:39:01 +0900

    ウィザード型式修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,6 +10,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
 
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
+  end
+
   def create_address
     @user = User.new(session["devise.regist_data"]["user"])
     @address = Address.new(address_params)
