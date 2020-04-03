abbd354d8939907e599bbe49f8e03ef79cd9b0e4
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-04-04 00:19:44 +0900

    [Fix] brandカラムの外部キー制約を解除

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_many :images
   belongs_to :user
-  belongs_to :brand
+  belongs_to :brand, optional: true
   belongs_to :category
 
   validates :name, :price, :detail, :status, :derivery_cost_id, :days_to_ship_id, :prefecture_id, presence: true
