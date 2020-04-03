94592dd494226b62ccf67ab132fbf321e40d229e
masayuki-tech <tennkuuno6@gmail.com>
2020-04-03 19:49:42 +0900

    パスワード文字数７文字以上に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable, password_length: 7..128
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, presence: true
   has_one :address
 end
