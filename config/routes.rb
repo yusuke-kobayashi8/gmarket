c08a9b45ae34494f889eabdbebcbeeba320fc80a
paradox398 <knife1341@gmail.com>
2020-04-03 14:40:16 +0900

    mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'products#index'
+  #root 'products#index'
+  root 'my_page#my_page'
+  #root 'may_page#my_page_logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
