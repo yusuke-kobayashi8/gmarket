233c07bdba3a81a1b8d8800a1874dbfaa50d01e7
masayuki-tech <tennkuuno6@gmail.com>
2020-04-04 13:46:49 +0900

    メールアドレス
    ＠とドメインがないと登録できないように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable, password_length: 7..128
+          :recoverable, :rememberable, :validatable, password_length: 7..128, email_regexp: /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, presence: true
   has_one :address
 end
