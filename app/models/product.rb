fa91541b6cdb1319ef43d85f8f77d4ad824f66bc
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-04-04 14:19:46 +0900

    [Add] productモデルにounerモデルとのアソシエーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :brand, optional: true
   belongs_to :category
+  has_one :order
 
   validates :name, :price, :detail, :status, :derivery_cost_id, :days_to_ship_id, :prefecture_id, presence: true
 end
