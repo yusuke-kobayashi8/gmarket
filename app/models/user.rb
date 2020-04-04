67982f724f96550065c288d1542e5ce60cb4c1c1
masayuki-tech <tennkuuno6@gmail.com>
2020-04-04 15:18:10 +0900

    名前の全角入力
    ファーストネームとファミリーネームが逆になっていたので修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable, password_length: 7..128, email_regexp: /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
+          :recoverable, :rememberable,
+          :validatable, password_length: 7..128,
+                        email_regexp: /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
+  validates_format_of :family_name, with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"
+  validates_format_of :first_name, with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"
+  validates_format_of :family_name_kana, with: /\A[ァ-ヶー－]+\z/
+  validates_format_of :first_name_kana, with: /\A[ァ-ヶー－]+\z/
+
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, presence: true
   has_one :address
 end
