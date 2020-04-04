8da749b40031684f73804b20193756e3d7b9e810
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-04-04 17:34:35 +0900

    [Add] 商品出品時にimagesも作成・保存できるように変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,11 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @product.images.new
   end
 
   def create
-    @product = Product.new
+    @product = Product.new(product_params)
     if @producut.save
       redirect_to root_path
     else
@@ -19,6 +20,6 @@ class ProductsController < ApplicationController
   private
 
     def product_params
-      params.require(:product).permit(:name, :price, :detail, :status, :derivery_fee, :derivery_days, :place, :category_id, :brand_id).merge(user_id: current_user.id)
+      params.require(:product).permit(:name, :price, :detail, :status, :derivery_fee, :derivery_days, :place, :category_id, :brand_id, images_attributes: [:image]).merge(user_id: current_user.id)
     end
 end
