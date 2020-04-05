1d5ee2b2dc273e514ea89397eec2f840449d411b
masayuki-tech <tennkuuno6@gmail.com>
2020-04-05 17:31:21 +0900

    エラーメッセージの修正と単体テストのための記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+    family_name           {"山田"}
+    first_name            {"太郎"}
+    family_name_kana      {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+    birthday              {"1990-02-02"}
+  end
+
+end
