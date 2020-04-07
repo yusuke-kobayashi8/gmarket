339fca997ad8cfe4e69993ff49525ea246132498
masayuki-tech <tennkuuno6@gmail.com>
2020-04-07 15:47:04 +0900

    微調整

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :postal_code, :prefectures, :city, :address, :posting_family_name, :posting_first_name, :posting_family_name_f, :posting_family_name_f, presence: true
+  validates :postal_code, :prefecture_id, :city, :address, :posting_family_name, :posting_first_name, :posting_family_name_f, :posting_family_name_f, presence: true
 end
