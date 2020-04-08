5ddc564329b1a5170f27dc769ef8f37116643cee
masayuki-tech <tennkuuno6@gmail.com>
2020-04-08 14:20:08 +0900

    レビュー後修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root 'home#index'
+  root 'products#index'
   resources :products, only: [:new, :create]
 end
