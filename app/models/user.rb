be29c74b8b57ea8fc0f344d6337a89fa5a1cbdcf
masayuki-tech <tennkuuno6@gmail.com>
2020-04-08 13:41:46 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
           :recoverable, :rememberable,
           :validatable, password_length: 7..128,
                         email_regexp:  /\A\S+@\S+\.\S+\z/
-  validates_format_of :family_name, with: /\A[ぁ-んァ-ン一-龥]/, message: "を全角で入力してください"
-  validates_format_of :first_name, with: /\A[ぁ-んァ-ン一-龥]/, message: "を全角で入力してください"
+  validates_format_of :family_name, with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "を全角で入力してください"
+  validates_format_of :first_name, with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "を全角で入力してください"
   validates_format_of :family_name_kana, with: /\A[ァ-ンー－]+\z/, message: "を全角で入力してください"
   validates_format_of :first_name_kana, with: /\A[ァ-ンー－]+\z/, message: "を全角で入力してください"
 
