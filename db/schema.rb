3dd3f630ef9eaf080c9f2861d5dd92472752230e
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-04-09 16:56:54 +0900

    [Add] 購入者、購入日時を保存するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -44,10 +44,13 @@ ActiveRecord::Schema.define(version: 2020_04_03_085517) do
     t.bigint "user_id"
     t.bigint "brand_id"
     t.bigint "category_id"
+    t.bigint "purchaser_id"
+    t.datetime "sold_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["purchaser_id"], name: "index_products_on_purchaser_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -67,4 +70,5 @@ ActiveRecord::Schema.define(version: 2020_04_03_085517) do
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
+  add_foreign_key "products", "users", column: "purchaser_id"
 end
