ac693b41643603ccdfdab71953921e1b107a8623
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-04-11 15:10:48 +0900

    [Fix] マイグレーションの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2020_04_08_020140) do
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
-  end
-
 ActiveRecord::Schema.define(version: 2020_04_03_085517) do
-  
-ActiveRecord::Schema.define(version: 2020_04_08_020140) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -76,10 +62,13 @@ ActiveRecord::Schema.define(version: 2020_04_08_020140) do
     t.bigint "user_id"
     t.bigint "brand_id"
     t.bigint "category_id"
+    t.bigint "purchaser_id"
+    t.datetime "sold_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["purchaser_id"], name: "index_products_on_purchaser_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -106,5 +95,5 @@ ActiveRecord::Schema.define(version: 2020_04_08_020140) do
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
-  
+  add_foreign_key "products", "users", column: "purchaser_id"
 end
