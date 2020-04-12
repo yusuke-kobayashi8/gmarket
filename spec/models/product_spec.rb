33e65cfe656c8962481de03a843127a7dae34151
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-04-12 15:42:18 +0900

    [Add] productモデルのspecファイル

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Product, type: :model do
+  describe '#create' do
+    context 'can save' do
+      it 'is valid with complete form'
+        
+    end
+  end
+end
