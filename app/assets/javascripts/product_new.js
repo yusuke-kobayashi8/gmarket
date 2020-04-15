e474f1f1aa42d8e9e89bc98e9ddd56d12223d934
masayuki-tech <tennkuuno6@gmail.com>
2020-04-15 21:45:42 +0900

    JSで複数画像の保存
    プレビューできる状態で削除も可能

--- app/assets/javascripts/product_new.js
+++ app/assets/javascripts/product_new.js
@@ -1,17 +1,62 @@
 $(function(){
-  
-  var file_field = document.querySelector('input[type=file]')
-  
-  $('#img-file').change(function(){
-    
-    var file = $('input[type="file"]').prop('files')[0];
-    var fileReader = new FileReader();
 
-    fileReader.onloadend = function() {
-      var src = fileReader.result
-      var html= `<img src="${src}" width="114" height="80">`
-      $('.seller__main__content__box__image').before(html);
+  var dataBox = new DataTransfer();
+  var file_field = document.querySelector('input[type=file]')
+
+  $('#img-file').change(function(){
+
+    var files = $('input[type="file"]').prop('files')[0];
+    $.each(this.files, function(i, file){
+
+      var fileReader = new FileReader();
+      dataBox.items.add(file)
+      file_field.files = dataBox.files
+      var num = $('.item-image').length + 1 + i
+      fileReader.readAsDataURL(file);
+      if (num == 10){
+        $('#seller__main__content__box__image').css('display', 'none')   
+      }
+
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= `<div class='item-image' data-image="${file.name}">
+                    <div class=' item-image__content'>
+                      <div class='item-image__content--icon'>
+                        <img src=${src} width="114" height="80" >
+                      </div>
+                    </div>
+                    <div class='item-image__operetion'>
+                      <div class='item-image__operetion--delete'>削除</div>
+                    </div>
+                  </div>`
+
+        $('#seller__main__content__box__image').before(html);
+      };
+
+      $('#seller__main__content__box__image').attr('class', `item-num-${num}`)
+    });
+  });
+
+  $(document).on("click", '.item-image__operetion--delete', function(){
+
+    var target_image = $(this).parent().parent()
+    var target_name = $(target_image).data('image')
+
+    if(file_field.files.length==1){
+      $('input[type=file]').val(null)
+      dataBox.clearData();
+      console.log(dataBox)
+    }else{
+      $.each(file_field.files, function(i,input){
+        if(input.name==target_name){
+          dataBox.items.remove(i) 
+        }
+      })
+      file_field.files = dataBox.files
     }
-    fileReader.readAsDataURL(file);
+    target_image.remove()
+    var num = $('.item-image').length
+    $('#seller__main__content__box__image').show()
+    $('#seller__main__content__box__image').attr('class', `item-num-${num}`)
   });
 });
