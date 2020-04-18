636b5e8e2b6f621072cb2e79318ad4f67a1f2ada
paradox398 <knife1341@gmail.com>
2020-04-18 15:38:41 +0900

    マイページビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -20,16 +20,15 @@ Rails.application.routes.draw do
 
   resources :creditcards, only: [:index, :new, :create, :destroy]
   
-  resources :mypage do
-    collection do
-      get :my_page
-      get :my_page_logout
-      get :my_page_credit
-      get :my_page_transaction
-      get :my_page_transactiond
-      get :my_page_exhibition
-      get :my_page_exhibiting
-      get :my_page_sold
-    end
-  end
+  resources :mypage, only: [:show]
+    #collection do
+      #get :my_page
+      #get :my_page_logout
+      #get :my_page_credit
+      #get :my_page_transaction
+      #get :my_page_transactiond
+      #get :my_page_exhibition
+      #get :my_page_exhibiting
+      #get :my_page_sold
+    #end
 end
