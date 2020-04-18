04061e19b005ccc39fceee6c68b15d65b2c3abcf
paradox398 <knife1341@gmail.com>
2020-04-18 22:22:21 +0900

    修正完了

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,5 +1,13 @@
 class MypageController < ApplicationController
+  before_action :authenticate_user!
+  before_action :certification
 
   def show
+  end
+
+  private
+
+  def certification
+    @user = User.find(params[:id])
   end
 end
