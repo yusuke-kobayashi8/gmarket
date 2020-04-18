83bc79416a9710578228c1a8acd0573a6c5f49a0
masayuki-tech <tennkuuno6@gmail.com>
2020-04-18 21:27:41 +0900

    編集クリック時にfile-fieldを実行

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
       get 'category_grandchildren', defaults: {format: 'json'}
     end
   end
- 
+  
   resources "category", only: [:index, :show]
 
   resources :creditcards, only: [:index, :new, :create, :destroy] do
