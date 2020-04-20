5d30f541f4a9cfb5e4021246f9bd13819d1d8802
masayuki-tech <tennkuuno6@gmail.com>
2020-04-20 13:21:35 +0900

    編集画面での削除完成

--- app/assets/javascripts/products_edit.js
+++ app/assets/javascripts/products_edit.js
@@ -1,4 +1,1 @@
-$(function(){
-  var target_image = $(this).parent().parent()
-  var target_name = $(target_image).data('image')
-});
+// ・});
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,6 +44,8 @@ class ProductsController < ApplicationController
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_update_params)
   end
 
   private
@@ -60,6 +62,12 @@ class ProductsController < ApplicationController
         :category_id,
         :brand_id,
         images_attributes: [:image]).merge(user_id: current_user.id)
+    end
+
+    def product_update_params
+      params.require(:product).permit(
+        :name,
+        [images_attributes: [:image, :_destroy, :id]])
     end
 
     def set_product
